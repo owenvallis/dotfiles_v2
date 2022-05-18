3d85e9846192cdf2e624fdf51e72a3a559532019
Owen Vallis <ovallis@google.com>
2022-05-18 13:39:49 -0700

    Update install_vimrc.sh

--- vim_runtime/install_vimrc.sh
+++ vim_runtime/install_vimrc.sh
@@ -6,7 +6,7 @@ set runtimepath+=~/.dotfiles/vim_runtime/after
 set runtimepath+=~/.fzf
 set packpath+=~/.dotfiles/vim_runtime
 source ~/.dotfiles/vim_runtime/vimrcs/vimrc.vim
-source ~/.dotfiles/vim_runtime/vimrcs/plugins_config.vim
-source ~/.dotfiles/vim_runtime/vimrcs/gvimrc.vim' > ${HOME}/.vimrc
+source ~/.dotfiles/vim_runtime/vimrcs/plugins_config.vim' > ${HOME}/.vimrc
+# source ~/.dotfiles/vim_runtime/vimrcs/gvimrc.vim
 
 echo "Installed the Vim configuration successfully!"
